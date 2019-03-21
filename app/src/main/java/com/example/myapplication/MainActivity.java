c3641b59664b7111f226060aefc579d2051108f9
o94488 <hu000079@algonquinlive.com>
2019-03-21 19:03:37 -0400

    lab3 not done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_profile);
+
+        Button nextPageButton = (Button)findViewById(R.id.nextPageButton);
+
+        nextPageButton.setOnClickListener( a -> {
+            Intent nextPage = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivityForResult(nextPage, 123);
+        });
+
     }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
 }
